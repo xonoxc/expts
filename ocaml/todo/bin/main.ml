d5fb7f82e5ff233ef7c1131b125f7aad2e12fae1
arpit <arpit2003y@gmail.com>
2026-09-13 22:49:37 +0530

    chore(ocaml):todo_done

--- ocaml/todo/bin/main.ml
+++ ocaml/todo/bin/main.ml
@@ -6,37 +6,40 @@
    specially if it's a single line comment
  comment *)
 
-type todo_status = Active | Done | InvalidStatus
+type todo_status =
+  | Active
+  | Done
+  | InvalidStatus
 
 let parse_todo_status (str : string) : todo_status =
-  match str with
+  match str |> String.trim with
   | "active" -> Active
   | "done" -> Done
   | _ -> InvalidStatus
+
+let parse_todo_status_opt (str : string) : todo_status option =
+  match str |> String.trim with
+  | "active" -> Some Active
+  | "done" -> Some Done
+  | _ -> None
 
 type todo_data = {
   id : int;
   mutable title : string;
   mutable desc : string;
-  status : todo_status;
+  mutable status : todo_status;
 }
 
 type update_todo_data = {
+  id : int;
   mutable title : string option;
   mutable desc : string option;
-  status : todo_status option;
+  mutable status : todo_status option;
 }
 
-type todo = Todo of todo_data | UpdateTodo of update_todo_data
-
-let todos = ref []
-
-type user_input =
-  | AddTodo of todo_data
-  | DeleteTodo of int
-  | Update of string option * string option * todo_status option
-  | Quit
-  | InvalidCmd
+type todo =
+  | Todo of todo_data
+  | UpdateTodo of update_todo_data
 
 let parse_todo_id (str_id : string) : int =
   match str_id |> String.trim |> int_of_string_opt with
@@ -53,52 +56,131 @@ let parse_valid_non_empty_string (str : string) (field_name : string) =
   | "" -> "[error] ${field_name} cannot be empty"
   | valid_str -> valid_str
 
-type update_todo = {
-  mutable title : string;
-  mutable desc : string;
-  status : todo_status;
-}
+let parse_valid_non_empty_string_opt (str : string) : string option =
+  match str |> String.trim with
+  | "" -> None
+  | valid_str -> Some valid_str
 
-let parse_todo (opt_id : string option) (title : string) (desc : string)
-    (status : string) : todo =
+let todos : todo_data list ref = ref []
+
+let parse_todo
+    (opt_id : string option) (title : string) (desc : string) (status : string)
+    : todo =
   let valid_id = opt_id |> parse_todo_id_opt in
-  let valid_title = title |> parse_valid_non_empty_string "title" in
-  let valid_desc = desc |> parse_valid_non_empty_string "desc" in
-  let valid_status = status |> parse_todo_status in
 
   match valid_id with
   | Some some_valid_id ->
-      Todo
+      UpdateTodo
         {
           id = some_valid_id;
-          title = valid_title;
-          desc = valid_desc;
-          status = valid_status;
+          title = title |> parse_valid_non_empty_string_opt;
+          desc = desc |> parse_valid_non_empty_string_opt;
+          status = status |> parse_todo_status_opt;
         }
   | None ->
-      UpdateTodo
-        { title = valid_title; desc = valid_desc; status = valid_status }
+      let new_id = !todos |> List.length in
+      Todo
+        ({
+           id = new_id;
+           title = parse_valid_non_empty_string title "title";
+           desc = parse_valid_non_empty_string desc "desc";
+           status = status |> parse_todo_status;
+         }
+          : todo_data)
+
+type user_input =
+  | AddTodo of todo_data
+  | DeleteTodo of int
+  | Update of int * string option * string option * todo_status option
+  | ShowTodos
+  | Quit
+  | InvalidCmd
+
+let status_to_string (status : todo_status) : string =
+  match status with
+  | Active -> "active"
+  | Done -> "done"
+  | InvalidStatus -> "?"
+
+let print_todos (todo_list : todo_data list) : unit =
+  match todo_list with
+  | [] -> print_endline "No todos yet."
+  | _ ->
+      print_endline "Todos:";
+      List.iter
+        (fun (todo : todo_data) ->
+          Printf.printf "  [%d] %s - %s (%s)\n" todo.id todo.title todo.desc
+            (status_to_string todo.status))
+        todo_list
 
 let parse_cmd (cmd : string) : user_input =
   match cmd |> String.trim |> String.split_on_char ' ' with
-  | [ "add"; id; title; desc; status ] -> (
-      let parsed_todo_data = parse_todo (Some id) title desc status in
+  (* add todo command  *)
+  | [ "add"; title; desc; status ] -> (
+      let parsed_todo_data = parse_todo None title desc status in
       match parsed_todo_data with
       | Todo data -> AddTodo data
-      | UpdateTodo _ -> InvalidCmd)
-  | [ "delete"; id ] -> DeleteTodo (parse_todo_id id)
-  | [ "update"; title; desc; status ] -> (
-      let update_todo_data = parse_todo None title desc status in
-      match update_todo_data with
-      | UpdateTodo data -> UpdateTodo data
-      | Todo _ -> InvalidCmd)
-  | [ "quit" ] -> Quit
+      | UpdateTodo _ -> InvalidCmd
+      (* delete todo by Id *))
+  | [ "delete"; id ] -> DeleteTodo (parse_todo_id id) (* update todo by Id *)
+  | "update" :: id :: rest ->
+      let title =
+        match List.nth_opt rest 0 with
+        | Some title -> parse_valid_non_empty_string_opt title
+        | None -> None
+      in
+      let desc =
+        match List.nth_opt rest 1 with
+        | Some desc -> parse_valid_non_empty_string_opt desc
+        | None -> None
+      in
+      let status =
+        match List.nth_opt rest 2 with
+        | Some status -> parse_todo_status_opt status
+        | None -> None
+      in
+      Update (parse_todo_id id, title, desc, status)
+  | [ "quit" ] -> Quit (* all other command are invalid *)
+  | [ "list" ]
+  | [ "show" ]
+  | [ "ls" ] ->
+      ShowTodos
   | _ -> InvalidCmd
 
 let () =
-  while true do
-    let input = read_line () |> parse_cmd in
+  try
+    while true do
+      print_endline "Enter a command (add, delete, update, list, quit):";
+      print_string "> ";
 
-    match input with
-    | AddTodo -> add_todo ()
-  done
+      let input = read_line () |> parse_cmd in
+
+      match input with
+      | AddTodo data -> todos := !todos @ [ data ]
+      | DeleteTodo id ->
+          todos := List.filter (fun (todo : todo_data) -> todo.id <> id) !todos
+      | Update (todo_id, todo_title, todo_desc, todo_status) -> (
+          match
+            List.find_opt (fun (todo : todo_data) -> todo.id = todo_id) !todos
+          with
+          | Some todo -> (
+              (match todo_title with
+              | Some title -> todo.title <- title
+              | None -> ());
+
+              (match todo_desc with
+              | Some desc -> todo.desc <- desc
+              | None -> ());
+
+              match todo_status with
+              | Some status -> todo.status <- status
+              | None -> ())
+          | None -> failwith "invalid todo id not found")
+      | ShowTodos -> print_todos !todos
+      | Quit -> raise Exit
+      | InvalidCmd ->
+          print_newline ();
+          print_string "Invalid command \n"
+    done
+  with
+  | Exit -> print_endline "Exiting ...."
